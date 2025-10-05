kinduz/SRTech-Chat

fe auth page start + eslint + shadcn

frontend/src/shared/index.ts
@@ -1,2 +1,4 @@
 export * from "./constants";
 export * from "./navigation";
+export * from "./components/toast";
+export * from "./hooks/use-toast";

kinduz/SRTech-Editor

frontend/src/shared/index.ts
@@ -1,2 +1,4 @@
 export * from "./constants";
 export * from "./navigation";
+export * from "./ui/toast";
+export * from "./hooks/use-toast";